1c08d2b4cec557b3494988055dcccd36d0ba25bb
Abdillahi485 <daakircadde143@gmail.com>
2023-10-29 14:19:40 +0300

    solved calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,21 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator(num1,num2,operator){
+    if(operator=== "+"){
+        console.log( num1 +num2)
+    }else if(operator=== "-"){
+        console.log( num1 -num2)
+    }else if(operator==="*"){
+        console.log( num1 * num2)
+    }else if (operator==="/"){
+        console.log( num1 / num2)
+    }else{
+        console.log( "invalid operator")
+    }
+
+}
+calculator(5,3,"+");
+calculator(5,3,"-");
+calculator(5,3, "*");
+calculator(5,3,"/");
+calculator (2,0,"?");
